3da8bff1ddcb38b2db52c6dc2610ee5742b4b4e3
Guilherme Garnier <guilherme.garnier@gmail.com>
2016-05-16 09:06:47 -0300

    Refactoring

--- src/felipe.js
+++ src/felipe.js
@@ -1,8 +1,31 @@
 var birthDay = new Date(2016, 4, 23);
 
+var pad = function(value) {
+  return value < 10 ? '0' + value : '' + value;
+};
+
+var processTime = function(remainingTime) {
+  var seconds = Math.round(remainingTime  / 1000);
+  var days = Math.floor(seconds / (60*60*24));
+  seconds -= (days*60*60*24);
+  var hours = Math.floor(seconds / (60*60));
+  seconds -= hours*60*60;
+  var minutes = Math.floor(seconds / 60);
+  seconds -= minutes*60;
+  seconds = Math.floor(seconds);
+
+  return {
+    days: pad(days),
+    hours: pad(hours),
+    minutes: pad(minutes),
+    seconds: pad(seconds)
+  };
+}
+
 var ProgressBar = React.createClass({
   render: function() {
     var className = 'c100 p' + this.props.percentage + ' big';
+
     return (
       <div className={className}>
         <span>{this.props.percentage}%</span>
@@ -16,21 +39,10 @@ var ProgressBar = React.createClass({
 });
 
 var ExampleApplication = React.createClass({
-  pad: function(value) {
-    return value < 10 ? '0' + value : '' + value;
-  },
-
   formatTime: function() {
-    var seconds = Math.round(this.props.remaining  / 1000);
-    var days = Math.floor(seconds / (60*60*24));
-    seconds -= (days*60*60*24);
-    var hours = Math.floor(seconds / (60*60));
-    seconds -= hours*60*60;
-    var minutes = Math.floor(seconds / 60);
-    seconds -= minutes*60;
-    seconds = Math.floor(seconds);
+    var time = processTime(this.props.remaining);
 
-    return this.pad(days) + ' dias ' + this.pad(hours) + ':' + this.pad(minutes) + ':' + this.pad(seconds);
+    return time.days + ' dias ' + time.hours + ':' + time.minutes + ':' + time.seconds;
   },
 
   render: function() {
